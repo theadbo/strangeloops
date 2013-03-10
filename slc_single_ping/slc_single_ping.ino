5be44d5c45f7fbc2054737c55dc892b6378d38e5
Cyndi Hill <adamhill@Cyndi-Hills-MacBook-Pro.local>
2013-03-10 01:27:39 -0500

    Changed output to AA#:###BB

--- slc_single_ping/slc_single_ping.ino
+++ slc_single_ping/slc_single_ping.ino
@@ -1,12 +1,9 @@
 #define PIR 7
+#define loop_delay 250
 
 int pirState = LOW;
 int val = 0;
 boolean executed = false;
-
-//uncomment when using ack
-int ack_track = 0;
-boolean ackFound = false;
 
 String pirStr, inputString = "";
 
@@ -19,46 +16,23 @@ void loop() {
   // read from ping sensors
   if (!executed) mySetup();
   
-//  uncomment when using ack  
-  int current_time = millis();
-  if ((current_time-ack_track)>10000){
-    software_Reset();
-  }
-
   val = ping(PIR);
   delay(5);
 
   
   //build serial strings for PINGs
-  pirStr = "0:";
+  pirStr = "AA0:";
   pirStr = pirStr + val;
-  pirStr = pirStr + "\r\n";
-
+  pirStr = pirStr + "BB";
   
-    //the following code implements the ACK method of data request
-    // print data to serial port, if requested
-  if (ackFound){
-    ack_track = millis();
-    Serial.print(pirStr);
-    delay(5);
-    // clear the serial output, reset the string and flag
-    Serial.flush();
-    inputString = "";
-    ackFound = false;
-  }
-//    // ************ //
-//    //comment out this code when using ACK
-//    Serial.print(pirStr);
-//    delay(5);
-//    Serial.print(pirStr2);
-//    delay(5);
-//    Serial.print(pirStr3);
-//    delay(5);
-//    Serial.print(flexStr);
-//    // clear the serial output, reset the string and flag
-//    Serial.flush();
-//    // **************//
-  delay(250);
+  //print the string
+  Serial.print(pirStr);
+  delay(5);
+  // clear the serial output, reset the string and flag
+  Serial.flush();
+  pirStr = "";
+
+  delay(loop_delay);
 }
 
 long ping(int pin) {
@@ -82,22 +56,6 @@ long microsecondsToCentimeters(long microseconds) {
 }
 
 
-// uncomment when using ack
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte:
-    char inChar = (char)Serial.read(); 
-    // add it to the inputString:
-    inputString += inChar;
-    // if the incoming character is a newline, check
-    // to see if the string was an ACK
-    if (inChar == '\n') {
-      if (inputString.equals("ACK\n"))
-      ackFound = true;
-    } 
-  }
-}
-
 void software_Reset() // Restarts program from beginning but does not reset the peripherals and registers
 {
 asm volatile ("  jmp 0");  
@@ -106,6 +64,7 @@ asm volatile ("  jmp 0");
 void mySetup(){
   pinMode(PIR, INPUT);
   Serial.begin(9600);
+  Serial.flush();
   executed = true;
   delay(500);
 }
